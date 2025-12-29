1cbb01da3235ee156c0acdd187e8eff749fc2e5b
jm325398-prog <jm325398@gmail.com>
2025-12-29 14:17:15 -0300

    Update rewards.js

--- rewards.js
+++ rewards.js
@@ -3,7 +3,7 @@
 // ============================================
 
 const CONFIG = {
-    API_URL: 'app/v2/recompensa/api.php',
+    API_URL: 'https://reword.great-site.net/index.php',
     MIN_SWIPE: 50,
     CARD_REMOVE_DELAY: 4000,
     ANIMATION_DELAY: 500
